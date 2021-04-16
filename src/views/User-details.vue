27e067d46fc07712391f378d54806298a70e3edb
AbbuTurab <syed.abbuturab@fissionlabs.com>
2021-04-16 17:45:22 +0530

    second commit

--- src/views/User-details.vue
+++ src/views/User-details.vue
@@ -2,19 +2,19 @@
 <div>
   <h3>This is User Details</h3>
   <label>Username</label>
-  <p>Hello {{getUser.name}}</p>
+  <p>Hello {{formUser.name}}</p>
   <label>Email</label>
-  <p>{{getUser.email}}</p>
+  <p>{{formUser.email}}</p>
 </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'Userdetails',
   computed: {
-    getUser() {
-      return this.$store.getters.formUser;
-    },
+    ...mapGetters(['formUser']),
   },
 };
 
